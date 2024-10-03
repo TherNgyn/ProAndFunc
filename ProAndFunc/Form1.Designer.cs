74c30e6baa7f930c1d0e7c9ff89a15b80b9ab5cb
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2024-10-03 18:46:20 +0700

    add

---
+++ ProAndFunc/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace ProAndFunc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            MaTS = new ColumnHeader();
+            NgonNgu = new ColumnHeader();
+            Bia = new ColumnHeader();
+            TrangThai = new ColumnHeader();
+            TuaSach = new ColumnHeader();
+            TacGia = new ColumnHeader();
+            TomTat = new ColumnHeader();
+            SLChuaMuon = new ColumnHeader();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { MaTS, NgonNgu, Bia, TrangThai, TuaSach, TacGia, TomTat, SLChuaMuon });
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(57, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(814, 122);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged_1;
+            // 
+            // MaTS
+            // 
+            MaTS.Text = "Mã Tựa Sách";
+            MaTS.Width = 100;
+            // 
+            // NgonNgu
+            // 
+            NgonNgu.Text = "Ngôn Ngữ";
+            NgonNgu.Width = 100;
+            // 
+            // Bia
+            // 
+            Bia.Text = "Bìa";
+            Bia.Width = 100;
+            // 
+            // TrangThai
+            // 
+            TrangThai.Text = "Trạng Thái";
+            // 
+            // TuaSach
+            // 
+            TuaSach.Text = "Tựa Sách";
+            TuaSach.Width = 100;
+            // 
+            // TacGia
+            // 
+            TacGia.Text = "Tác Giả";
+            TacGia.Width = 100;
+            // 
+            // TomTat
+            // 
+            TomTat.Text = "Tóm Tắt";
+            TomTat.Width = 150;
+            // 
+            // SLChuaMuon
+            // 
+            SLChuaMuon.Text = "Số Lượng Chưa Mượn";
+            SLChuaMuon.Width = 100;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(777, 165);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Chọn";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(245, 167);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(526, 27);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 170);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Nhập ISBN - Mã Đầu Sách";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(245, 365);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(526, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(777, 364);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Chọn";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 368);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Nhập Mã Tựa Sách";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(60, 219);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(811, 122);
+            dataGridView1.TabIndex = 7;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(953, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private ListView listView1;
+        
+        private TextBox textBox1;
+        
+        private ColumnHeader MaTS;
+        private ColumnHeader NgonNgu;
+        private ColumnHeader Bia;
+        private ColumnHeader TrangThai;
+        private ColumnHeader TuaSach;
+        private ColumnHeader TacGia;
+        private ColumnHeader TomTat;
+        private ColumnHeader SLChuaMuon;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label2;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ ProAndFunc/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProAndFunc
+{
+    public partial class Form1 : Form
+    {
+        String strCon = @"Data Source=DESKTOP-0PJCAJ8\SQLEXPRESS;Initial Catalog=ThuVien;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+        SqlConnection sqlCon = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HienThiThongTin(string isbn)
+        {
+            try
+            {
+                if (sqlCon == null)
+                {
+                    sqlCon = new SqlConnection(strCon);
+                }
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlCommand sqlCom = new SqlCommand();
+                // Gọi thủ tục: pro_lietKe
+                sqlCom.CommandType = CommandType.StoredProcedure;
+                sqlCom.CommandText = "pro_lietKe";
+                sqlCom.Connection = sqlCon;
+                // đưa giá trị của isbn vào tham số tên @isbn
+                sqlCom.Parameters.AddWithValue("@isbn", isbn);
+                // Thực thi câu lệnh gọi thủ tục và trả về kết quả
+                SqlDataReader sqlRe = sqlCom.ExecuteReader();
+                // Xóa dữ liệu cũ trong listView1
+                listView1.Items.Clear();
+                // Đọc dữ liệu từ sqlRe và hiển thị lên listView1
+                if (!sqlRe.HasRows)
+                {
+                    MessageBox.Show("Không tìm thấy sách", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                while (sqlRe.Read())
+                {
+                    // sqlRe["ma_tuasach"] là cột ma_tuasach trong kết quả trả về
+                    // ToString () chuyển giá trị của cột ma_tuasach sang kiểu chuỗi
+                    ListViewItem item = new ListViewItem(sqlRe["ma_tuasach"].ToString());
+                    // SubItems.Add thêm các cột còn lại vào item
+                    item.SubItems.Add(sqlRe["ngonngu"].ToString());
+                    item.SubItems.Add(sqlRe["bia"].ToString());
+                    item.SubItems.Add(sqlRe["trangthai"].ToString());
+                    item.SubItems.Add(sqlRe["tuasach"].ToString());
+                    item.SubItems.Add(sqlRe["tacgia"].ToString());
+                    item.SubItems.Add(sqlRe["tomtat"].ToString());
+                    item.SubItems.Add(sqlRe["SLSachChuaMuon"].ToString());
+                    // Thêm item vào listView1
+                    listView1.Items.Add(item);
+                }
+                sqlRe.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Lấy giá trị của ISBN từ textBox1
+            string isbn = textBox1.Text;
+            if (!string.IsNullOrEmpty(isbn))
+            {
+                // Gọi hàm HienThiThongTin với tham số là isbn
+                HienThiThongTin(isbn);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập ISBN hợp lệ.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string tuasach = textBox2.Text;
+            if (!string.IsNullOrEmpty(tuasach))
+            {
+                DemSoCuonSach(tuasach);
+            }
+            
+            else
+            {
+                MessageBox.Show("Vui lòng nhập tựa sách hợp lệ.");
+            }
+        }
+
+        private void DemSoCuonSach(string tuasach)
+        {
+            try
+            {
+                if (sqlCon == null)
+                {
+                    sqlCon = new SqlConnection(strCon);
+                }
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlCommand sqlComm = new SqlCommand();
+                sqlComm.CommandType = CommandType.Text;
+                sqlComm.CommandText = "SELECT * FROM dbo.TongSoCuonSach(@tuasach)";
+                sqlComm.Connection = sqlCon;
+                sqlComm.Parameters.AddWithValue("@tuasach", tuasach);
+                
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlComm);
+
+                    // Tạo đối tượng DataTable để lưu trữ dữ liệu
+                DataTable dataTable = new DataTable();
+
+                    // Điền dữ liệu vào DataTable
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("Không tìm thấy tựa sách", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Gán DataTable làm nguồn dữ liệu cho DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
